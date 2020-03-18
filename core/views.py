7e3f802124d33bf9a5540fb62de5782944c1037a
AnderPlascencia <anderplascencia@gmail.com>
2020-03-17 18:43:03 -0600

    checkout address saved

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, View
-from .models import Item, Order, OrderItem
+from .models import Item, Order, OrderItem, BillingAddress
 from django.utils import timezone
 from django.shortcuts import redirect
 from django.core.exceptions import ObjectDoesNotExist
@@ -20,13 +20,31 @@ class CheckoutView(View):
 
     def post(self, *args, **kwargs):
         form = CheckoutForm(self.request.POST or None)
-        print(self.request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-            print("The form is valid")
+
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            if form.is_valid():
+                street_address = form.cleaned_data.get('street_address')
+                apartment_address = form.cleaned_data.get('apartment_address')
+                country = form.cleaned_data.get('country')
+                zip_code = form.cleaned_data.get('zip_code')
+                # same_billing_address = form.cleaned_data.get(
+                #     'same_billing_address')
+                # save_info = form.cleaned_data.get('save_info')
+                payment_option = form.cleaned_data.get('payment_option')
+                billing_address = BillingAddress(
+                    street_address=self.request.user,
+                    apartment_address=street_address,
+                    country=country,
+                    zip_code=zip_code
+                )
+                billing_address.save()
+                return redirect("core:checkout")
+            messages.warning(self.request, "The form failed to post ")
             return redirect("core:checkout")
-        messages.warning(self.request, "The form failed to post ")
-        return redirect("core:checkout")
+        except ObjectDoesNotExist:
+            messages.error(self.request, "You do not have items in your cart")
+            return redirect("core:order-summary")
 
 
 class HomeView(ListView):
@@ -42,7 +60,6 @@ class ItemDetailView(DetailView):
 
 class OrderSummaryView(LoginRequiredMixin, View):
     def get(self, *args, **kwargs):
-
         try:
             order = Order.objects.get(user=self.request.user, ordered=False)
             context = {
